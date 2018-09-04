8ee86709410f60171635fc54beb0c052757b86b7
lbarret4 <llewellynbar4@gmail.com>
2018-09-04 03:10:27 -0400

    add textbox and button with events to app component

--- my-first-react-ap/src/components/App.jsx
+++ my-first-react-ap/src/components/App.jsx
@@ -9,11 +9,46 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            text:"some text"
+            text:" with some text",
+            hasLoaded:false
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
+
+    handleChange(e){
+        this.setState({
+            text:e.target.value
+        })
+    }
+
+    handleClick(){
+        this.setState({
+            hasLoaded:!this.state.hasLoaded
+        });
+    }
+
     render(){
-        return <h1>This is my fist react {this.props.type} element with {this.state.text} . </h1>;
+        if(this.state.hasLoaded){
+            return ( 
+                <React.Fragment>
+                <h1>{this.props.text} {this.state.text}. </h1> 
+                <input type="text" onChange={this.handleChange}></input>
+                <button onClick={this.handleClick} style={{display:"block"}}>button</button>
+                </React.Fragment>);           
+
+        }else{
+            return( 
+                <React.Fragment>
+                <h1>Loading..................</h1>
+                <button onClick={this.handleClick} style={{display:"block"}}>button</button>
+                </React.Fragment>
+                // conse
+            );
+
+        }
+   
+    
     }
 }
 export default App;
