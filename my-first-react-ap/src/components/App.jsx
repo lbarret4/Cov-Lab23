458ecd216b0e79a9d9de561425c6927b90a39822
lbarret4 <llewellynbar4@gmail.com>
2018-09-04 00:38:07 -0400

    Change functional component to class-based with state

--- my-first-react-ap/src/components/App.jsx
+++ my-first-react-ap/src/components/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-function App(props){
+// function App(props){
 
-    return <h1>This is my fist react {props.type} element. </h1>;
+//     return <h1>This is my fist react {props.type} element. </h1>;
+// }
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            text:"some text"
+        }
+    }
+    render(){
+        return <h1>This is my fist react {this.props.type} element with {this.state.text} . </h1>;
+    }
 }
-
 export default App;
