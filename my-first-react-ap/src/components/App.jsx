ea586678f2f2026f14a08736ab9cc143d575b7da
lbarret4 <llewellynbar4@gmail.com>
2018-09-04 11:51:30 -0400

    completed with bonus described in comments

--- my-first-react-ap/src/components/App.jsx
+++ my-first-react-ap/src/components/App.jsx
@@ -9,8 +9,10 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            text:" with some text",
+            text:"with some text",
+            //text:this.props.text,  value is undefine if props is not passed in to component's and super's constructor
             hasLoaded:false
+         
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
@@ -40,14 +42,14 @@ class App extends Component {
                 <React.Fragment>
                 <h1>{this.props.text} {this.state.text}. </h1> 
                 <input type="text" onChange={this.handleChange}></input>
-                <button onClick={this.handleClick} style={{display:"block"}}>button</button>
+                <button onClick={this.handleClick} style={{display:"block"}}>Load button</button>
                 </React.Fragment>);           
 
         }else{
             return( 
                 <React.Fragment>
                 <h1>Loading..................</h1>
-                <button onClick={this.handleClick} style={{display:"block"}}>button</button>
+                <button onClick={this.handleClick} style={{display:"block"}}>Load button</button>
                 </React.Fragment>
                 // conse
             );
